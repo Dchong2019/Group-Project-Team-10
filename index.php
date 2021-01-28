07723e0d9b4761aa87e4c28b42eeafecd9a7131b
redrey333 <73810730+redrey333@users.noreply.github.com>
2021-01-27 21:40:23 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+    <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+  <title>About</title>
+  <script type="text/javascript" src="script/validation.min.js"></script>
+<script type="text/javascript" src="script/ajax.js"></script>
+
+  <body>
+    <header>About Me</header>
+    <h1>Redrick V. Horton-Schittko</h1>
+    <p>As one can guess, I am Redrick. I'm a decent person who loves video games and anime. In college, I follow the Computer Science path because I do have aspirations of one day becoming a game developer. I'm an overall calm person who loves to have fun and hang out, which is why I enjoy the color Ocean Blue. If anyone ever meets me and wants to start up a conversation, feel free to. I will apologize, though, for my conversation skills.</p>
+  </body>
+</html>
