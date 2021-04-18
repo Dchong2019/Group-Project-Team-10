02db4daa1dfbb58008728ee7b9b7baa1935e3618
Trinity Carnegie <54779644+CarnegieT@users.noreply.github.com>
2021-04-18 13:00:20 -0400

    Add files via upload

---
+++ User Search Function/DatabaseConnect.php
@@ -0,0 +1,8 @@
+
+<?php
+    define("$DB_SERVER", "sql206.epizy.com");
+    define("$DB_USER", "epiz_2794108");
+    define("$DB_PASS", "Solidsnake42");
+    define("$DB_NAME", "epiz_27941068_loginsystem");
+
+?>
