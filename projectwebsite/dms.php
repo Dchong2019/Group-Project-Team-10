7356a1fb3ea7117aecb5f3b12044f51bcd8ac27e
redrey333 <73810730+redrey333@users.noreply.github.com>
2021-04-19 12:34:56 -0400

    Add files via upload

---
+++ projectwebsite/dms.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Home</title>
+<meta charset="UTF-8" name="" content="">
+<link rel="stylesheet" href="styles5.css">
+</head>
+<body>
+  <div id="chat_box">
+    <p1 id="display"></p1>
+    <p2 id="text_box"></p2>
+    <p3 id="send">Send</p3>
+  </div>
+</body>
+</html>
